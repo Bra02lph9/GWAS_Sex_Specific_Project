60867309d429ca1b71dca577c709fb1e5774b4e5
DATA.BANK(ILYASS.GH) <157898132+IlyassCODEX@users.noreply.github.com>
2026-05-07 21:02:44 +0100

    Add files via upload

---
+++ src/comparison/compare_pathways.py
@@ -0,0 +1,91 @@
+import pandas as pd
+
+# =========================
+# LOAD FILES
+# =========================
+
+female_df = pd.read_csv("../GWAS_Sex_Specific_Project/3_tools_results/gprofiler/female_pathways.tsv")
+male_df = pd.read_csv("../GWAS_Sex_Specific_Project/3_tools_results/gprofiler/male_pathways.tsv")
+
+# =========================
+# KEEP IMPORTANT COLUMNS
+# =========================
+
+columns_to_keep = [
+    "term_name",
+    "source",
+    "adjusted_p_value",
+    "intersection_size"
+]
+
+female_df = female_df[columns_to_keep]
+male_df = male_df[columns_to_keep]
+
+# =========================
+# EXTRACT PATHWAY SETS
+# =========================
+
+female_pathways = set(female_df["term_name"])
+male_pathways = set(male_df["term_name"])
+
+# =========================
+# COMPARE PATHWAYS
+# =========================
+
+shared_pathways = female_pathways.intersection(male_pathways)
+
+female_specific = female_pathways - male_pathways
+male_specific = male_pathways - female_pathways
+
+# =========================
+# CREATE OUTPUT DATAFRAMES
+# =========================
+
+shared_df = pd.concat([
+    female_df[female_df["term_name"].isin(shared_pathways)],
+    male_df[male_df["term_name"].isin(shared_pathways)]
+]).drop_duplicates()
+
+female_specific_df = female_df[
+    female_df["term_name"].isin(female_specific)
+]
+
+male_specific_df = male_df[
+    male_df["term_name"].isin(male_specific)
+]
+
+# =========================
+# SAVE RESULTS
+# =========================
+
+shared_df.to_csv("shared_pathways.csv", index=False)
+
+female_specific_df = female_specific_df.sort_values("adjusted_p_value")
+female_specific_df.to_csv(
+    "female_specific_pathways.csv",
+    index=False
+)
+
+male_specific_df = male_specific_df.sort_values("adjusted_p_value")
+male_specific_df.to_csv(
+    "male_specific_pathways.csv",
+    index=False
+)
+
+# =========================
+# SUMMARY
+# =========================
+
+print("===== PATHWAY COMPARISON =====")
+
+print(f"Female pathways: {len(female_pathways)}")
+print(f"Male pathways: {len(male_pathways)}")
+
+print(f"Shared pathways: {len(shared_pathways)}")
+print(f"Female-specific pathways: {len(female_specific)}")
+print(f"Male-specific pathways: {len(male_specific)}")
+
+print("\nFiles generated:")
+print("- shared_pathways.csv")
+print("- female_specific_pathways.csv")
+print("- male_specific_pathways.csv")
